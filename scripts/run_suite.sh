fd616291780b719e1f5baa4802c634a6dba00b24
Erik Heinz <jerik@gmx.net>
2018-11-14 22:35:10 +0100

    can pass now tags in run_test.sh, ex: --include foo

--- scripts/run_suite.sh
+++ scripts/run_suite.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
 set -e
 
+# ex: ./run_test.sh 
 CMD="robot --console verbose --exclude once --outputdir /reports /suites"
 
-if [[ "$@" = "once" ]]; then 
-	# this will run the testcases with the tag "once"
-	# In this testcase I create a random new user with the login credetials of suites/newuser.txt
-	# the user is then activated and can be used by the other tests, where the testcases with tag "once" are excluded
-	echo ":: RUN ONCE"
-	zufall=$( openssl rand -hex 4 )
-	rduser=bot-${zufall}
-	echo '*** Variables ***' > suites/newuser.txt
-	echo "\${USER}    ${rduser}" >> suites/newuser.txt
-	echo "\${EMAIL}    ${rduser}@mailinator.com" >> suites/newuser.txt
-	echo "\${USERPW}    ${zufall}#Mii" >> suites/newuser.txt
+# Test if variabel is empty
+if [[ ! -z "$@" ]]; then 
 
-	CMD="robot --console verbose --include once --outputdir /reports /suites"
+	if [[ "$@" = "once" ]]; then 
+		# ex: ./run_test.sh once
+		# this will run the testcases with the tag "once"
+		# In this testcase I create a random new user with the login credetials of suites/newuser.txt
+		# the user is then activated and can be used by the other tests, where the testcases with tag "once" are excluded
+		echo ":: RUN ONCE"
+		zufall=$( openssl rand -hex 4 )
+		rduser=bot-${zufall}
+		echo '*** Variables ***' > suites/newuser.txt
+		echo "\${USER}    ${rduser}" >> suites/newuser.txt
+		echo "\${EMAIL}    ${rduser}@mailinator.com" >> suites/newuser.txt
+		echo "\${USERPW}    ${zufall}#Mii" >> suites/newuser.txt
+		CMD="robot --console verbose --include once --outputdir /reports /suites"
+	else 
+		# ex: ./run_test.sh --include capture
+		# ex: ./run_test.sh --exclude capture
+		CMD="robot --console verbose $@ --outputdir /reports /suites"
+	fi
 fi
 
 echo Script run with arguments :: "$@"
